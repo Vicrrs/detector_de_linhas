e70a667824e7a81a86bbac0e13407e2620986f5c
Vicrrs <enricofermi2019@gmail.com>
2022-09-22 14:35:56 -0400

    Terceiro commit

--- lane_detection.py
+++ lane_detection.py
@@ -1,6 +1,18 @@
 import cv2
 import numpy as np
 
+def draw_the_lines(image, lines):
+
+    lines_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), thickness=3)
+
+    image_with_lines = cv2.addWeighted(image, 0.8, lines_image, 1, 0.0)
+
+    return image_with_lines
+
 
 def region_of_interest(image, region_points):
     mask = np.zeros_like(image)
@@ -19,14 +31,19 @@ def get_detected_lanes(image):
 
     region_of_interest_vertices = [
         (0, height),
-        (width/2, height*0.5),
+        (width/2, height*0.65),
         (width, height)
     ]
 
     cropped_image = region_of_interest(
         canny_image, np.array([region_of_interest_vertices], np.int32))
 
-    return cropped_image
+    lines = cv2.HoughLinesP(cropped_image, rho=2, theta=1 * np.pi / 180,
+                           threshold=50, lines=np.array([]), minLineLength=100, maxLineGap=150)
+
+    image_with_lines = draw_the_lines(image, lines)
+
+    return image_with_lines
 
 # link para o vídeo (https://www.kaggle.com/datasets/dpamgautam/video-file-for-lane-detection-project?resource=download)
 # várias imagens de quadros mostradas uma após a outra
@@ -42,7 +59,7 @@ while video.isOpened():
     frame = get_detected_lanes(frame)
 
     cv2.imshow('Detector de linha', frame)
-    cv2.waitKey(15)
+    cv2.waitKey(50)
 
 video.release()
 cv2.destroyAllWindows()
