30134a96f505932d87247c17782a80e0cd6c551f
Vicrrs <enricofermi2019@gmail.com>
2022-09-22 10:33:00 -0400

    Segundo commit

--- lane_detection.py
+++ lane_detection.py
@@ -2,16 +2,35 @@ import cv2
 import numpy as np
 
 
+def region_of_interest(image, region_points):
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, region_points, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+
+    return masked_image
+
+
 def get_detected_lanes(image):
     (height, width) = (image.shape[0], image.shape[1])
-    
+
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     canny_image = cv2.Canny(gray_image, 100, 120)
 
-    return canny_image
+    region_of_interest_vertices = [
+        (0, height),
+        (width/2, height*0.5),
+        (width, height)
+    ]
 
+    cropped_image = region_of_interest(
+        canny_image, np.array([region_of_interest_vertices], np.int32))
+
+    return cropped_image
+
+# link para o vídeo (https://www.kaggle.com/datasets/dpamgautam/video-file-for-lane-detection-project?resource=download)
 # várias imagens de quadros mostradas uma após a outra
+
 video = cv2.VideoCapture('test_video.mp4')
 
 while video.isOpened():
